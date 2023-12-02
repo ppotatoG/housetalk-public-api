5a3007dc3f536743c6e76b8c6678694dfabb27a5
ppotatoG <thgus2270@gmail.com>
2023-12-02 14:39:37 +0900

    Feat: 거리에 따른 리턴값 변경

--- src/utils/calculateWalkingTime.ts
+++ src/utils/calculateWalkingTime.ts
@@ -1,4 +1,9 @@
-export const calculateWalkingTime = (distance: number) => {
-  const walkingSpeed = 100;
-  return Math.ceil(distance / walkingSpeed);
+export const calculateWalkingDistance = (
+  distance: number
+): 5 | 10 | 15 | 20 | null => {
+  if (distance <= 500) return 5;
+  if (distance <= 1000) return 10;
+  if (distance <= 1500) return 15;
+  if (distance <= 2000) return 20;
+  return null;
 };
