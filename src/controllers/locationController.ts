f4d97c141bbdb9aa56c8bfb99f7a144b37064433
ppotatoG <thgus2270@gmail.com>
2023-12-02 14:41:22 +0900

    Refactor: 파라미터 변경

--- src/controllers/locationController.ts
+++ src/controllers/locationController.ts
@@ -1,14 +1,12 @@
 import { Request, Response } from 'express';
 import { searchNearbyFacilities } from '../services/locationService';
-import { WALKING_LIMIT } from '../constants/location';
 
 export const findNearbyPlaces = async (req: Request, res: Response) => {
   try {
-    const { latitude, longitude } = req.body;
+    const { latitude, longitude } = req.query;
     const places = await searchNearbyFacilities(
-      latitude,
-      longitude,
-      WALKING_LIMIT
+      Number(latitude),
+      Number(longitude)
     );
 
     res.json(places);
