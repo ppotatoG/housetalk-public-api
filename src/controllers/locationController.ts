1c1a8aa525dc811526f2d2c958291517933e0da9
ppotatoG <thgus2270@gmail.com>
2023-12-02 15:48:29 +0900

    Refactor: tsconfig paths

--- src/controllers/locationController.ts
+++ src/controllers/locationController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import { searchNearbyFacilities } from '../services/locationService';
+import { searchNearbyFacilities } from '@/services/locationService';
 
 export const findNearbyPlaces = async (req: Request, res: Response) => {
   try {
--- src/routes/locationRoutes.ts
+++ src/routes/locationRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { findNearbyPlaces } from '../controllers/locationController';
+import { findNearbyPlaces } from '@/controllers/locationController';
 
 const router = express.Router();
 
