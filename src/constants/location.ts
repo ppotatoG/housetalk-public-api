9778b1ace6a26d5f46d32d58b27ffd2d6aa22c17
ppotatoG <thgus2270@gmail.com>
2023-12-03 16:29:16 +0900

    Refactor: 상수 구조 변경

--- src/constants/location.ts
+++ src/constants/location.ts
@@ -1,25 +1,23 @@
-import { Define } from '../types/location';
+import { Define } from '@/types/location';
 
 export const DEFINE: Define = {
-  CATEGORY_GROUP_CODE: {
-    SUPERMARKET: 'MT1',
-    CONVENIENCE_STORE: 'CS2',
-    SCHOOL: 'SC4',
-    PARKING: 'PK6',
-    GAS_STATION: 'OL7',
-    CAFE: 'CE7',
-    SUBWAY: 'SW8',
-    BANK: 'BK9',
-    HOSPITAL: 'HP8',
-    PHARMACY: 'PM9',
-  },
-  QUERY: {
-    STARBUCKS: '스타벅스',
-    DAISO: '다이소',
-    BUS_STOP: '버스 정류장',
-    OLLIVE_YOUNG: '올리브영',
-    PC_ROOM: 'PC방',
-    CINEMA: '영화관',
+  ITEMS: {
+    SUPERMARKET: { type: 'category', id: 'MT1', name: '대형마트' },
+    CONVENIENCE_STORE: { type: 'category', id: 'CS2', name: '편의점' },
+    SCHOOL: { type: 'category', id: 'SC4', name: '학교' },
+    PARKING: { type: 'category', id: 'PK6', name: '주차장' },
+    GAS_STATION: { type: 'category', id: 'OL7', name: '주유소' },
+    CAFE: { type: 'category', id: 'CE7', name: '카페' },
+    SUBWAY: { type: 'category', id: 'SW8', name: '지하철역' },
+    BANK: { type: 'category', id: 'BK9', name: '은행' },
+    HOSPITAL: { type: 'category', id: 'HP8', name: '병원' },
+    PHARMACY: { type: 'category', id: 'PM9', name: '약국' },
+
+    STARBUCKS: { type: 'keyword', id: '스타벅스', name: '스타벅스' },
+    DAISO: { type: 'keyword', id: '다이소', name: '다이소' },
+    OLLIVE_YOUNG: { type: 'keyword', id: '올리브영', name: '올리브영' },
+    PC_ROOM: { type: 'keyword', id: 'PC방', name: 'PC방' },
+    CINEMA: { type: 'keyword', id: '영화관', name: '영화관' },
   },
 };
 
